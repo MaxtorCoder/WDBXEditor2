30dbe0afde5d7e3e97eb85e56bde262174f2c338
Luzifix <luzifix19@gmail.com>
2020-05-03 04:55:43 +0200

    Implement definition version select

---
+++ WDBXEditor2/Misc/JsonSettings.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace WDBXEditor2.Misc
+{
+    public class JsonSettings
+    { /// <summary>
+      /// The settings class fetches the settings.json from the local installation folder.
+      /// It only reads the file on startup and writes on close. 
+      /// If you plan on editing the Settings file, edit '_settings' directly.
+      /// </summary>
+        private static string SettingsFile { get; set; }
+        private static JObject _settings = new JObject();
+
+        /// <summary>
+        /// Initializes a new instance of a settings file at requested location.
+        /// </summary>
+        /// <param name="location">Location of the file.</param>
+        public JsonSettings(string location)
+        {
+            SettingsFile = location;
+            try
+            {
+                _settings = new JObject(SettingsObject);
+            }
+            catch
+            {
+                var jo = new JObject();
+                jo["settings"] = new JObject();
+                File.WriteAllText(SettingsFile, jo.ToString());
+                _settings = new JObject();
+            }
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                return (String)_settings[key];
+            }
+            set
+            {
+                if (_settings[key] == null)
+                    AddSetting(key, value);
+                else
+                    ChangeSetting(key, value);
+            }
+        }
+
+        /// <summary>
+        /// Adds the specified key to the settings JObject.
+        /// </summary>
+        /// <param name="tokenKey"></param>
+        /// <param name="value"></param>
+        public void AddSetting(string tokenKey, string value)
+        {
+            _settings.Add(tokenKey, value);
+        }
+
+        /// <summary>
+        /// Adds the specified bool value to the settings JObject.
+        /// </summary>
+        /// <param name="tokenKey"></param>
+        /// <param name="value"></param>
+        public void AddSetting(string tokenKey, bool value)
+        {
+            _settings.Add(tokenKey, value);
+        }
+
+        /// <summary>
+        /// Changes the string value of the specified key.
+        /// If the key doesn't exist, it will create it.
+        /// </summary>
+        /// <param name="tokenKey"></param>
+        /// <param name="value"></param>
+        public void ChangeSetting(string tokenKey, string value)
+        {
+            if (_settings[tokenKey] != null)
+            {
+                _settings[tokenKey].Replace(value);
+                return;
+            }
+            _settings.Add(tokenKey, value);
+        }
+
+        /// <summary>
+        /// Changes the bool value of the specified key.
+        /// If the key doesn't exist, it will create it.
+        /// </summary>
+        /// <param name="tokenKey"></param>
+        /// <param name="value"></param>
+        public void ChangeSetting(string tokenKey, bool value)
+        {
+            if (_settings[tokenKey] != null)
+            {
+                _settings[tokenKey].Replace(value);
+                return;
+            }
+            _settings.Add(tokenKey, value);
+        }
+
+        /// <summary>
+        /// Removes the setting from the settings JObject.
+        /// </summary>
+        /// <param name="tokenKey"></param>
+        public void RemoveSetting(string tokenKey)
+        {
+            if (_settings[tokenKey] != null)
+                _settings[tokenKey] = null;
+        }
+
+        /// <summary>
+        /// Saves the settings JObject to the file.
+        /// </summary>
+        public void Save()
+        {
+            var saveObject = new JObject();
+            saveObject["settings"] = _settings ?? new JObject();
+            File.WriteAllText(SettingsFile, saveObject.ToString());
+        }
+
+        /// <summary>
+        /// Clear the previous settings file to default.
+        /// </summary>
+        public void Clear()
+        {
+            _settings.RemoveAll();
+            Save();
+        }
+
+        /// <summary>
+        /// Fetches the text from the JSON file and parses it to 
+        /// the settings JObject.
+        /// </summary>
+        private static JObject SettingsObject
+        {
+            get
+            {
+                var lines = File.ReadAllLines(SettingsFile);
+                var line = "";
+
+                //begins scanning lines
+                foreach (var l in lines.Where(l => !l.StartsWith("//")))
+                {
+                    var i = 0;
+                    var isReadingComment = false;
+                    var passing = "";
+
+                    //begins scanning line for commented string. 
+                    foreach (var c in l)
+                    {
+                        if (c == '/' && l[i++] == '*' && !isReadingComment) isReadingComment = true;
+                        if (c == '*' && l[i++] == '/' && isReadingComment) isReadingComment = false;
+                        if (!isReadingComment) passing += c;
+                        i++;
+                    }
+                    line += passing;
+                }
+
+                var o = JObject.Parse(line);
+                return (JObject)o["settings"];
+            }
+        }
+    }
+}
---
+++ WDBXEditor2/Views/DefinitionSelect.xaml.cs
@@ -0,0 +1,126 @@
+﻿using DBCD;
+using DBDefsLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using WDBXEditor2.Misc;
+using static DBDefsLib.Structs;
+
+namespace WDBXEditor2.Views
+{
+    public partial class DefinitionSelect : Window
+    {
+        private List<DefinitionSelectData> DefinitionSelectData = new List<DefinitionSelectData>()
+        {
+            new DefinitionSelectData() { DisplayName = "Automatic select", Build = null }
+        };
+
+        private List<LocalSelectData> LocalSelectData = new List<LocalSelectData>()
+        {
+            new LocalSelectData() { DisplayName = "enUS", Locale = Locale.EnUS },
+            new LocalSelectData() { DisplayName = "enGB", Locale = Locale.EnGB },
+            new LocalSelectData() { DisplayName = "koKR", Locale = Locale.KoKR },
+            new LocalSelectData() { DisplayName = "frFR", Locale = Locale.FrFR },
+            new LocalSelectData() { DisplayName = "deDE", Locale = Locale.DeDE },
+            new LocalSelectData() { DisplayName = "enCN", Locale = Locale.EnCN },
+            new LocalSelectData() { DisplayName = "zhCN", Locale = Locale.ZhCN },
+            new LocalSelectData() { DisplayName = "enTW", Locale = Locale.EnTW },
+            new LocalSelectData() { DisplayName = "zhTW", Locale = Locale.ZhTW },
+            new LocalSelectData() { DisplayName = "esES", Locale = Locale.EsES },
+            new LocalSelectData() { DisplayName = "esMX", Locale = Locale.EsMX },
+            new LocalSelectData() { DisplayName = "ruRU", Locale = Locale.RuRU },
+            new LocalSelectData() { DisplayName = "ptPT", Locale = Locale.PtPT },
+            new LocalSelectData() { DisplayName = "ptBR", Locale = Locale.PtBR },
+            new LocalSelectData() { DisplayName = "itIT", Locale = Locale.ItIT }
+        };
+
+        public bool IsCanceled = false;
+        public string SelectedVersion = null;
+        public Locale SelectedLocale = Locale.EnUS;
+
+        public DefinitionSelect()
+        {
+            InitializeComponent();
+            DefinitionSelectList.Focus();
+            DefinitionSelectList.ItemsSource = DefinitionSelectData;
+            LocaleSelectList.ItemsSource = LocalSelectData;
+
+            string lastLocaleSelectedIndexSetting = SettingStorage.Get("LastLocaleSelectedIndex");
+            if (lastLocaleSelectedIndexSetting != null)
+            {
+                int lastLocaleSelectedIndex = Int32.Parse(lastLocaleSelectedIndexSetting);
+                LocaleSelectList.SelectedIndex = lastLocaleSelectedIndex;
+            }
+        }
+
+        private void Load_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedVersion = (DefinitionSelectList.SelectedValue as DefinitionSelectData).Build;
+            IsCanceled = false;
+            Close();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedVersion = null;
+            IsCanceled = true;
+            Close();
+        }
+
+        public void SetDefinitionFromVersionDefinitions(VersionDefinitions[] versionDefinitions)
+        {
+            foreach (VersionDefinitions versionDefinition in versionDefinitions)
+            {
+                if (versionDefinition.buildRanges.Length > 0)
+                {
+                    foreach (BuildRange buildRange in versionDefinition.buildRanges)
+                    {
+                        DefinitionSelectData.Add(new DefinitionSelectData()
+                        {
+                            DisplayName = String.Format("{0} - {1}", buildRange.minBuild, buildRange.maxBuild),
+                            Build = buildRange.maxBuild.ToString()
+                        });
+                    }
+                }
+                else
+                {
+                    foreach (Build build in versionDefinition.builds)
+                    {
+                        DefinitionSelectData.Add(new DefinitionSelectData()
+                        {
+                            DisplayName = build.ToString(),
+                            Build = build.ToString()
+                        });
+                    }
+                }
+            }
+
+            DefinitionSelectList.ItemsSource = DefinitionSelectData.OrderBy(e => e.Build).ToList();
+        }
+
+        private void LocaleSelected(object sender, SelectionChangedEventArgs e)
+        {
+            LocalSelectData localSelectData = (LocaleSelectList.SelectedItem as LocalSelectData);
+
+            if (localSelectData.Locale != SelectedLocale)
+            {
+                SelectedLocale = localSelectData.Locale;
+                SettingStorage.Store("LastLocaleSelectedIndex", LocaleSelectList.SelectedIndex.ToString());
+            }
+        }
+    }
+
+    class DefinitionSelectData
+    {
+        public string DisplayName { get; set; }
+        public string Build { get; set; } = null;
+    }
+
+    class LocalSelectData
+    {
+        public string DisplayName { get; set; }
+        public Locale Locale { get; set; }
+    }
+}
